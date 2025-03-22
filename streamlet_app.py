cbb222fb1795463823281ba2899687ac905eb431
omarabdelhady802 <omarabdelhady802@gmail.com>
2025-03-22 17:25:57 +0200

    streamlit deploing

---
+++ streamlet_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Flask Template in Streamlit")
+
+# Flask URL (ensure Flask is running)
+flask_url = "http://127.0.0.1:5000"
+
+# Embed the Flask-rendered page
+st.components.v1.iframe(flask_url, height=600, scrolling=True)
